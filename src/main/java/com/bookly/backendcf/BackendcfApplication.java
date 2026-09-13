d160cb63c828ca1739678256606b3253b9f30275
Elena <ele92gris@gmail.com>
2026-09-13 18:02:52 -0500

    init repo

---
+++ src/main/java/com/bookly/backendcf/BackendcfApplication.java
@@ -0,0 +1,13 @@
+package com.bookly.backendcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcfApplication.class, args);
+	}
+
+}
